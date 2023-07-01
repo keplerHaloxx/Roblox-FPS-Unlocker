0f65030420d463279810d8fcdbd335f247ff4b86
keplerHaloxx <christian1soro@gmail.com>
2023-07-01 22:49:06 +1000

    .exe ver support (hopefully)

--- src/actions.py
+++ src/actions.py
@@ -1,5 +1,6 @@
 import os
 import time
+from sys import _MEIPASS
 from pathlib import Path
 
 def progress_print(output) -> None:
@@ -36,7 +37,7 @@ def unlock_fps() -> None:
     while not Path.exists(Path(roblox_folder, "ClientSettings")):
         time.sleep(.1)
 
-    with open("ClientAppSettings.json", "r") as file:
+    with open(os.path.join(sys._MEIPASS, "ClientAppSettings.json"), "r") as file:
         # Gets text from ClientAppSettings.json
         text = file.read()
         with open(Path(roblox_folder, "ClientSettings", "ClientAppSettings.json"), "w+") as file2:
